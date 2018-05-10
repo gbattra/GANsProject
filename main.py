2b662689f652d5bd976111b55660e7c5b16f18b4
Greg Attra <greg.attra@gmail.com>
2018-05-09 22:51:06 -0400

    optimizers and training built

--- main.py
+++ main.py
@@ -83,4 +83,77 @@ def generator(z, batch_size, z_dim):
 
     return g4
 
+z_dimensions = 100
+z_placeholder = tf.placeholder(tf.float32, [None, z_dimensions])
 
+generated_image_output = generator(z_placeholder, 1, z_dimensions)
+z_batch = np.random.normal(0, 1, [1, z_dimensions])
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    generated_image = sess.run(generated_image_output, feed_dict={z_placeholder: z_batch})
+    generated_image = generated_image.reshape([28, 28])
+    plt.imshow(generated_image, cmap='Greys')
+    plt.show()
+
+tf.reset_default_graph()
+batch_size = 50
+
+z_placeholder = tf.placeholder(tf.float32, [None, z_dimensions], name='z_placeholder')
+x_placeholder = tf.placeholder(tf.float32, [None, 28, 28, 1], name='x_placeholder')
+
+Gz = generator(z_placeholder, batch_size, z_dimensions)
+Dx = discriminator(x_placeholder)
+
+Dg = discriminator(Gz, reuse=True)
+
+d_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=Dx, labels=tf.ones_like(Dx)))
+# we're not classifying here, so Y=1 for all examples
+
+d_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=Dg, labels=tf.zeros_like(Dg)))
+# we're not classifying here, so Y=0 for all examples
+
+g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=Dg, labels=tf.ones_like(Dg)))
+
+tvars = tf.trainable_variables()
+d_vars = [var for var in tvars if 'd_' in var.name]
+g_vars = [var for var in tvars if 'g_' in var.name]
+
+print([v.name for v in d_vars])
+print([v.name for v in g_vars])
+
+# set up training for discriminator
+with tf.variable_scope('fake-optimizer', reuse=tf.AUTO_REUSE):
+    d_trainer_fake = tf.train.AdamOptimizer(0.0003).minimize(d_loss_fake, var_list=d_vars)
+
+with tf.variable_scope('real-optimizer', reuse=tf.AUTO_REUSE):
+    d_trainer_real = tf.train.AdamOptimizer(0.0003).minimize(d_loss_real, var_list=d_vars)
+
+# set up training for the generator
+with tf.variable_scope('generator-optimizer', reuse=tf.AUTO_REUSE):
+    g_trainer = tf.train.AdamOptimizer(0.0001).minimize(g_loss, var_list=g_vars)
+
+# TENSOR BOARD
+tf.summary.scalar('Generator_loss', g_loss)
+tf.summary.scalar('Discriminator_loss_real', d_loss_real)
+tf.summary.scalar('Discriminator_loss_fake', d_loss_fake)
+
+images_for_tensorboard = generator(z_placeholder, batch_size, z_dimensions)
+tf.summary.image('Generated_images', images_for_tensorboard, 5)
+merged = tf.summary.merge_all()
+logdir = "tensorboard/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + "/"
+writer = tf.summary.FileWriter(logdir, sess.graph)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+# pre-train discriminator
+for i in range(300):
+    z_batch = np.random.normal(0, 1, size=[batch_size, z_dimensions])
+    real_image_batch = mnist.train.next_batch(batch_size)[0].reshape([batch_size, 28, 28, 1])
+    _, __, dLossReal, dLossFake = sess.run(
+        [d_trainer_real, d_trainer_fake, d_loss_real, d_loss_fake],
+        {x_placeholder: real_image_batch, z_placeholder: z_batch}
+    )
+    if (i % 100 == 0):
+        print("dLossReal:", dLossReal, "dLossFake:", dLossFake)
